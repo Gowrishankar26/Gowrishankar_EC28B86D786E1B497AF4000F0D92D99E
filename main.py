48c690fa1a1cf217764d5ff5f649c01605880cff
Gowrishankar26 <144438996+Gowrishankar26@users.noreply.github.com>
2023-09-23 08:57:56 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-#Implement a recursive function to #calculate the factorial of a given
-#numbe
-def fact_rec(n): 
-  if n ==0 or n ==1 : 
-    return 1
-        
-  else:
-    return n*fact_rec(n-1)
-number = int(input ("Enter a value :  "))
-res = fact_rec(number)
-print("The factorial of {} is {}.".format(number, res))
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+
+def sort_students(student_list):
+    # Sort the list of students based on their CGPA in descending order
+    sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
+    return sorted_students
+# Create a list of student objects
+students = [
+    Student("Alice", "A001", 3.8),
+    Student("Bob", "B002", 3.5),
+    Student("Charlie", "C003", 3.9),
+    Student("David", "D004", 3.7),
+]
+
+# Sort the list of students by CGPA
+sorted_students = sort_students(students)
+
+# Print the sorted list
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
